d9abcf4cddfca7b5ddcc8261cc5e63f502f62b91
root <gabriel3052000@yahoo.com>
2020-11-03 15:44:04 -0800

    add W3 css. Decoupled navbar to be used else where

---
+++ public/javascripts/sidebar.js
@@ -0,0 +1,9 @@
+function openNav(){
+  document.getElementById("userHomeSidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("userHomeSidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
