8ffc6543c44882b2b7064c7c34ed39d4da4064a1
dgghun <gabriel3052000@yahoo.com>
2020-11-06 23:34:46 -0800

    added phone input check

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,3 +3,19 @@
  * Type: Javascripts
  * Description: generic app scripts
  */
+
+ function checkPhone(){
+    // Jquery to check input
+    $('#phone').on('input', function(e){
+        var phone = document.getElementById('phone').value
+        phone = phone.replace(/\D/g,'')
+
+        if(phone.length >= 10){
+            phone = phone.replace(/[-]/g,'')
+            var tmp = phone.substring(0,3) + "-" + phone.substring(3,6) + "-" + phone.substring(6,10)
+            phone = tmp
+        }
+
+        document.getElementById('phone').value = phone
+    })
+}
