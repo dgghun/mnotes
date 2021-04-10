de3cea06cd70026bea7ce3e11b089130fb4fb735
dgghun <gabriel3052000@yahoo.com>
2021-04-10 16:49:11 -0700

    added datatables paths to layout and app.js

--- app.js
+++ app.js
@@ -44,6 +44,9 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/selectize/dist/js/standalone')))
 app.use('/css', express.static(path.join(__dirname, 'node_modules/selectize/dist/css')))
 app.use('/css', express.static(path.join(__dirname, 'node_modules/selectize-bootstrap4-theme/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/datatables.net/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/datatables.net-dt/js')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/datatables.net-dt/css')))
 app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
 
